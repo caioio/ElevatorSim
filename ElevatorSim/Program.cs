1b48910aa75193f2187ee0dcf727c99058e276f6
CaioBurns <caioburns@ucl.br>
2021-11-11 20:46:11 -0300

    Adicionar arquivos de projeto.

---
+++ ElevatorSim/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using log4net;
+
+namespace ElevatorSim
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Logger log = new Logger();
+            log.Log_load();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+    class Logger
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public void Log_load()
+        {
+            log.Info("Aplicação iniciada.");
+        }
+    }
+}
